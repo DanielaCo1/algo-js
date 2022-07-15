4cad56d182f97ba22e4e876311f25be1f5c70060
corda daniela <danielacorda.pro@gmail.com>
2022-07-15 15:46:28 +0200

    ex

--- 2.3.js
+++ 2.3.js
@@ -4,5 +4,10 @@
          console.log(counter);
          counter += 1;
     }
-
+    /*
+    
+    for (let counter = 1; counter <= 50; counter +=1) {
+        console.log(counter);
+    
+    }
     
--- 2.4.js
+++ 2.4.js
@@ -1,11 +1,10 @@
-let counter = 1;
-let counter = 2;
-let counter = 3;
+  
 
-    while (counter <= 100) {
-        console.log(counter);
-        counter += 1;
-        controle.log(counter);
-        counter += 2;
-        controle.log(counter);
-        counter +=3;
+    for (let counter = 1; counter <= 100;  counter+= 1 ) {
+      if (counter % 2 ==0) {
+        console.log(counter / 2);
+        } else {
+            console.log(counter * 3);
+             }
+    } 
+    
